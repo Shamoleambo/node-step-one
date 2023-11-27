3f5eee55bdf18518ffe71089aeb16530b9c4c677
Tiago Gomes <tidgomes@gmail.com>
2023-11-27 11:34:57 -0300

    send some responses

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req)
+  console.log(req.url, req.method, req.headers)
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>É nois!</title></head>')
+  res.write('<body>')
+  res.write('<h1>Title</h1><p>Hello from my NodeJS server!</p>')
+  res.write('</body>')
+  res.write('</html>')
+  res.end()
 })
 
 server.listen(3000)
