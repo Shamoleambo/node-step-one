0669220de0b46b7000afc5f835e992b65c07fecf
Tiago Gomes <tidgomes@gmail.com>
2023-11-27 11:56:13 -0300

    add routing

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers)
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<html>')
-  res.write('<head><title>É nois!</title></head>')
-  res.write('<body>')
-  res.write('<h1>Title</h1><p>Hello from my NodeJS server!</p>')
-  res.write('</body>')
-  res.write('</html>')
-  res.end()
+  const { url, method, body } = req
+  if (url === '/') {
+    res.write(`<html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Form</title>
+    </head>
+    <body>
+      <form action="/message" method="POST">
+        <input type="text" name="mano" />
+        <button type="submit">Send</button>
+      </form>
+    </body>`)
+    res.end()
+  } else if (url === '/message' && method === 'POST') {
+    res.write(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <title>Message</title>
+    </head>
+    <body>
+      <h1>Messages</h1>
+    </body>
+    </html>
+    `)
+    res.end()
+  }
 })
 
 server.listen(3000)
